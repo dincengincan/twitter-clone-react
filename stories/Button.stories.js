a9ab2212f1855c392b6db0756770d09994ead360
dincengincan <engincandinc@gmail.com>
2020-08-15 19:17:22 +0300

    Add storybook

---
+++ stories/Button.stories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import  Button  from '../components/Button';
+
+export default {
+  title: 'Example/Button',
+  component: Button,
+};
+
+export const SampleButton = () => <Button>Deneme</Button>
+
+
+
