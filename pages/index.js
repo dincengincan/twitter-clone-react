04179c621549244e636595651c746cff6f8b2708
dincengincan <engincandinc@gmail.com>
2020-09-26 19:08:40 +0300

    Add reset.css

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
+import Button from "../components/Button"
+
 function HomePage() {
-    return <div>Welcome to Next.js!</div>
+    return (
+    <div>
+      Welcome to Next.js!
+      <Button>DENEME</Button>
+      </div>
+    )
   }
   
   export default HomePage
