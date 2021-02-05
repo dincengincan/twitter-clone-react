dc35310100ea27a213dcd88bafd10d59e571f312
dincengincan <engincandinc@gmail.com>
2021-02-05 16:34:52 +0300

    Add fullWidth prop to Button component

--- components/Button.js
+++ components/Button.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 import styles from './Button.module.css';
 
-const Button = ({ children, ...props }) => (
-  <button type="button" className={styles.button} {...props}>
+const Button = ({ fullWidth, children, className }) => (
+  <button
+    type="button"
+    className={
+      styles.button +
+      (fullWidth ? ` ${styles.fullWidth}` : '') +
+      (className ? ` ${className}` : '')
+    }
+  >
     {children}
   </button>
 );
